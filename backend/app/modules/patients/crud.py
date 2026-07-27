4ce8c53b84ee0f348a9194834d2930ceeb3e3dae
Mario Merino Baena <mariomerinodev@MacBook-Pro-de-Mario.local>
2026-07-28 01:22:43 +0200

    feat: métodos get para obtener pacientes completados correctamente

--- backend/app/modules/patients/crud.py
+++ backend/app/modules/patients/crud.py
@@ -4,6 +4,7 @@ from . import schemas
 from . import models
 
 
+# Función para crear un nuevo paciente
 def patient_create(db: Session, patient: schemas.PatientCreate):
     # 1. Comprobar si ya existe el paciente en la db
     patient_existing = db.query(models.Patient).filter(models.Patient.legal_identifier == patient.legal_identifier).first()
@@ -26,3 +27,18 @@ def patient_create(db: Session, patient: schemas.PatientCreate):
     db.commit()
     db.refresh(new_patient)
     return new_patient
+
+
+# Función para obtener todos los pacientes
+def patient_get_list(db: Session):
+    db_patients = db.query(models.Patient).all()
+    return db_patients
+
+# Función para obtener un paciente
+def patient_get(db: Session, patient_id: int):
+    # 1. Comprobar si existe el paciente
+    db_patient = db.query(models.Patient).filter(models.Patient.id == patient_id).first()
+    
+    if not db_patient:
+        return None
+    return db_patient
--- backend/app/modules/patients/router.py
+++ backend/app/modules/patients/router.py
@@ -1,3 +1,4 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from ...core.database import get_db
@@ -14,3 +15,21 @@ def patient_create(patient: schemas.PatientCreate, db: Session = Depends(get_db)
     if db_patient == None:
         raise HTTPException(status_code=400, detail="El paciente ya está registrado ")
     return db_patient
+
+# GET -> Obtener todos los pacientes
+@router.get("/", response_model=List[schemas.PatientResponse])
+def patient_get_list(db: Session = Depends(get_db)):
+    return crud.patient_get_list(db=db)
+
+# GET -> Obtener un paciente
+@router.get("/{patient_id}", response_model=schemas.PatientResponse)
+def patient_get(patient_id: int, db: Session = Depends(get_db)):
+    db_patient = crud.patient_get(db=db, patient_id=patient_id)
+
+    if db_patient == None:
+        raise HTTPException(status_code=404, detail="El paciente no existe")
+    return db_patient
+
+# PATCH -> Actualizar datos de un paciente
+
+# DELETE -> Eliminar un paciente
